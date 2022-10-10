8d142411535406a2c2ab0e796a8b40bcad800cd2
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2022-10-10 13:04:15 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,15 +4,15 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 
 export default {
   port: parseInt(process.env.PORT ? process.env.PORT : '3000'),
-  dbURL: process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost:27017/coreyo',
+  dbURL: process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost:27017/JustAMongodbDatabase',
   session: {
     name: 'SID',
-    secret: 'SID',
+    secret: 'SID',    // Make a secret key complex
     cookie: {
       httpOnly: true,
       secure: false,
       maxAge: 365 * 24 * 60 * 60 * 1000,
     },
   },
-  newsAPIKey: '085cd41830184321ada726cb58773294',
+  newsAPIKey: '?',   // you can get news api key on https://newsapi.org/
 };
